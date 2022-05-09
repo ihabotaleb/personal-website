c92c5a6e558f6ade27fa2c68235403714ddaac95
Ibrahim Abotaleb <ibrahimabotaleb@Ibrahims-MacBook-Pro.local>
2022-05-10 00:33:41 +0200

    Add base dragable

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
+import Draggable from 'react-draggable';
 
 const About:NextPage = () => {
     return (
@@ -11,8 +12,24 @@ const About:NextPage = () => {
             <main>
                 <h1>About</h1>
                 <Link href="/">Back to index</Link>
+
+                <div className="w-[100vw] h-[100vh]">
+                <Draggable
+                    // axis="y"
+                    defaultPosition={{x: 20, y: 20}}  >
+                    <div className="bg-orange-500 ">
+                        <p>
+                            yes!
+                        </p>
+                        <h3>
+                            Definitely
+                        </h3>
+                    </div>
+                </Draggable>
+                </div>
             </main>
         </div>
+    
     );
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,27 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
+import { useWindowSize } from "usehooks-ts";
 
 const Home: NextPage = () => {
 
+  const { width, height } = useWindowSize();
   
+  // TODO: Add responsive offsets
+  let svgXStartOffset = (height > width) ? 47.5 : 48.5;
+
   return (
     <div>
       <Head>
         <title>Ibrahim Abotaleb</title>
         <meta name="description" content="Personal website for Ibrahim Abotaleb" />
+	      <meta name="theme-color" content="#7b1d06" />
       </Head>
       <main>
         <div className="behind grid grid-cols-6 grid-rows-6 w-[100vw] h-[100vh]">
-          {/* <div className="col-start-3 col-end-5 row-start-3 row-end-5"> */}
           <div className="col-start-1 col-end-7 row-start-1 row-end-7">
             <svg width="100%" height="100%">
-              <svg x="47%" y="50%" overflow="visible">
+              <svg x={"" + svgXStartOffset + "%"} y="50%" overflow="visible">
                 <g transform="rotate(12)">
                   <ellipse cx={0} cy={0} rx={170} ry={100} fill="none" stroke="black" />
                 </g>
