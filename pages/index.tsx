97879cc74a9863ac012508bb32a3d8a06734536a
Ibrahim Abotaleb <ibrahimabotaleb@Ibrahims-MacBook-Pro.local>
2022-05-10 03:42:03 +0200

    Make draft home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,19 +7,21 @@ const Home: NextPage = () => {
 
   const { width, height } = useWindowSize();
 
-
-  let svgXStartOffset = 50;
-
   let polys = [];
   for (let i = 0; i < 300 ; i++) {
 
-    let sign = (Math.random() > 0.5) ? 1:-1;
-    let r = Math.random() * width * 1.5 * sign; 
+    let x = Math.random() * width *2 - width / 2 ; 
+    let y = Math.random() * height;
 
-    let l = Math.random() * height * 0.35;
+    let r = Math.round(Math.random()*150+105);
+    let g = Math.round(Math.random()*150+105);
+    let b = Math.round(Math.random()*150+105);
+
+    let yprime = height * 1.3;
+    let thickness = y/200;
 
       polys.push(
-        <polygon points={"" + (r-r/10) +","+ (l-l/10) + " " + (r-r/10) +","+ (l+l/10) + " " + 1*sign*(-1) + ",0"} fill={"rgb("+parseInt(Math.random()*200_55)+","+parseInt(Math.random()*200+55)+","+parseInt(Math.random()*200+55)+")"} stroke="None"/>
+        <polygon points={"" + (x) +","+ (y-thickness) + " " + (x) +","+ (y+thickness) + " " + width / 2 + ","+yprime} fill={"rgb("+r+","+g+","+b+")"} stroke="None"/>
     );
   }
 
@@ -33,16 +35,12 @@ const Home: NextPage = () => {
       <main>
         <div className="behind grid grid-cols-6 grid-rows-6 w-[100vw] h-[100vh]">
           <div className="col-start-1 col-end-7 row-start-1 row-end-7">
-            <svg width="100%" height="100%" className="opacity-[0.7]" fill='none'>
-              <svg x={"" + svgXStartOffset + "%"} y="60%" overflow="visible">
-                <g>
+            <svg width="100%" height="100%" className="opacity-[0.7] bg-transparent blur-[0.0px]" fill='none'>
                   {polys}
-                </g>
-              </svg>
             </svg>
           </div>
         </div>
-        <div className="grid grid-cols-6 grid-rows-6 w-[100vw] h-[100vh]">
+        <div className="grid grid-cols-6 grid-rows-6 w-[100vw] h-[100vh] ">
               <div className="col-start-2 col-end-4 row-start-3 row-end-5">
                 <h1 className="title flex-1 boxcent  h-[100%] w-[100%] mr-4">Ibrahim Abotaleb</h1>
               </div>
