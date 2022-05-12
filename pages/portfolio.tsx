be51cb5a687e911cca181ffc9c46eaee22d27cf8
ihabotaleb <i.h.abotaleb@gmail.com>
2022-05-12 13:15:55 +0200

    Add image gallery to portfolio page

--- pages/portfolio.tsx
+++ pages/portfolio.tsx
@@ -2,9 +2,30 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import { useWindowSize } from "usehooks-ts";
 import Header from "./components/header";
+import Gallery from "react-grid-gallery";
 
 const Portfolio: NextPage = () => {
   const { width, height } = useWindowSize();
+  
+  const IMAGES = [{
+      src: "imgs/sq1.png",
+      thumbnail: "imgs/sq1.png",
+      thumbnailWidth: 300,
+      thumbnailHeigh: 400,
+      caption: "Wrektacular pour"
+  }, {src: "imgs/sq1.png",
+  thumbnail: "imgs/sq1.png",
+  thumbnailWidth: 300,
+  thumbnailHeigh: 400,
+  caption: "Wrektacular pour"},{src: "imgs/sq1.png",
+  thumbnail: "imgs/sq1.png",
+  thumbnailWidth: 300,
+  thumbnailHeigh: 400,
+  caption: "Wrektacular pour"},{src: "imgs/sq1.png",
+  thumbnail: "imgs/sq1.png",
+  thumbnailWidth: 300,
+  thumbnailHeigh: 400,
+  caption: "Wrektacular pour"}];
 
   return (
     <div>
@@ -19,7 +40,8 @@ const Portfolio: NextPage = () => {
       <main>
         <div className="subPage">
           <Header />
-          <p className="italic text-center">CONTENT TO BE PUT HERE</p>
+          <p className="italic text-center">PORTFOLIO CONTENT INTRO TO BE PUT HERE</p>
+            <Gallery images={IMAGES} />
         </div>
       </main>
     </div>
