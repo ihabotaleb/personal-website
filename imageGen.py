772a2952f24ac6c6fb803e99fa22ef7d4d672241
ihabotaleb <i.h.abotaleb@gmail.com>
2022-05-13 02:14:04 +0200

    Add portfolio image text python script generator

--- imageGen.py
+++ imageGen.py
@@ -4,16 +4,28 @@ import glob
 # Get all image paths
 paths = glob.glob("public/imgs/*.png")
 
-outString = "IMAGES=["
+imgPaths = "const IMAGES=["
 
 for path in paths:
     im = Image.open(path)
     width, height = im.size
     
-    outString += "{ "
-    outString += "\n  src:'" + path[7:] + "',"
-    outString += "\n  width:" + str(width) + ","
-    outString += "\n  height:" + str(height) + "\n},"
+    imgPaths += "{ "
+    imgPaths += "src:'" + path[7:] + "',"
+    imgPaths += "width:" + str(width) + ","
+    imgPaths += "height:" + str(height) + "},"
 
-outString = outString[:-1] + "];"
-print(outString)
+imgPaths = imgPaths[:-1] + "];\n"
+print(imgPaths)
+
+f = open("pages/portfolio.tsx", "r")
+txt = f.readlines()
+f.close()
+
+del txt[7]
+txt.insert(7, imgPaths)
+
+f = open("pages/portfolio.tsx", "w")
+txt = "".join(txt)
+f.write(txt)
+f.close()
--- pages/portfolio.tsx
+++ pages/portfolio.tsx
@@ -5,15 +5,7 @@ import PhotoAlbum from "react-photo-gallery";
 
 const Portfolio: NextPage = () => {
   
-  const IMAGES=[{ 
-    src:'imgs/rw1.png',
-    width:876,
-    height:322
-  },{ 
-    src:'imgs/sq1.png',
-    width:1000,
-    height:1017
-  }];
+const IMAGES=[{ src:'imgs/PrepartyPoster.png',width:1446,height:1872},{ src:'imgs/White Mockup.png',width:2634,height:1376},{ src:'imgs/WREK Vines.png',width:1372,height:1110},{ src:'imgs/Variant 5.png',width:2550,height:3300},{ src:'imgs/Variant 8.png',width:2550,height:3300},{ src:'imgs/sq1.png',width:1000,height:1017},{ src:'imgs/PrepartyPoster0.png',width:1628,height:2090},{ src:'imgs/Swirl WIP.png',width:1360,height:762}];
   
   return (
     <div>
@@ -28,8 +20,7 @@ const Portfolio: NextPage = () => {
       <main>
         <div className="subPage">
           <Header />
-          <p className="italic text-center">PORTFOLIO CONTENT INTRO TO BE PUT HERE</p>
-            <PhotoAlbum photos={IMAGES}/>
+          <PhotoAlbum photos={IMAGES}/>
         </div>
       </main>
     </div>
