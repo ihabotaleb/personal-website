606f6e431a4727154d4bf75217b5b4f0a0ef970b
Ibrahim Abotaleb <iawad@synapse-analytics.io>
2023-05-20 19:10:31 +0300

    Update about bio

--- pages/about.tsx
+++ pages/about.tsx
@@ -19,7 +19,15 @@ const About: NextPage = () => {
           <div className="flex flex-col">
             <p className="neue aboutBigText mt-5 pl-[16.5%] pr-[16.5%]">
               {
-                "I'm a developer with a passion in making tools that help people create. I love media. I love making things. I love designing things. Growing up, I went from learning scripting with the Adobe Creative Suite, to sandboxing in game engines like Unity, and even researching how realtime rendering was achieved. I always questioned how media I consumed was able to be generated and experienced. That's why you'll always find me messing around with new graphics technologies and seeing what cool experiments lead to."
+                `
+                I'm a passionate developer with a relentless drive to create tools that empower people to unleash their creativity. My love for media, design, and the art of making things has been a constant thread throughout my life. From early experiences scripting in the Adobe Creative Suite to exploring game engines like Unity and diving into the fascinating world of real-time rendering, I've always been captivated by how media is generated and experienced.
+
+With a diverse range of skills and a natural curiosity, I'm constantly pushing the boundaries of what's possible. I thrive on experimenting with new graphics technologies and embarking on cool projects that blend innovation with creativity.
+
+Combining my academic pursuits with a Bachelor's degree in Computer Science, with a focus on Intelligence and Media, I've gained a solid foundation in areas such as data structures, algorithms, computer graphics, machine learning, information visualization, and computer vision. Currently pursuing a Master's degree in Computer Science, with a specialization in Computer Graphics, I'm eager to explore the forefront of graphics technologies and discover new ways to create impactful experiences.
+
+Driven by a strong sense of curiosity, creativity, and an innate desire to make a difference, I'm excited to bring my skills and personality to innovative projects.
+                `
               }
             </p>
           </div>
